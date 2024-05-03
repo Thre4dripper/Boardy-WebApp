7dad012bffe13ce602a704761f611c634c96901e
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-05-03 20:23:16 +0530

    added line tool functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Pen from '@/app/components/Pen';
+import Line from '@/app/components/line';
 
 export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const mouseRef = useRef({ x: 0, y: 0, down: false });
 
   const [pens, setPens] = useState<Pen[]>([]);
+  const [lines, setLines] = useState<Line[]>([]);
 
   const initCanvas = useCallback(() => {
     if (!canvasRef.current) return;
@@ -15,29 +17,42 @@ export default function Home() {
     canvas.height = window.innerHeight;
   }, []);
 
-  const draw = useCallback((drawFn: () => void) => {
-    const canvas = canvasRef.current as HTMLCanvasElement;
+  const draw = useCallback(
+    (drawFn: () => void) => {
+      const canvas = canvasRef.current as HTMLCanvasElement;
 
-    if (!canvas) return;
-    const ctx = canvas.getContext('2d');
+      if (!canvas) return;
+      const ctx = canvas.getContext('2d');
 
-    if (!ctx) return;
+      if (!ctx) return;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    //draw pens
-    pens.forEach((pen) => {
-      ctx.strokeStyle = 'black';
-      ctx.lineWidth = 5;
-      ctx.beginPath();
-      ctx.moveTo(pen.path[0].x, pen.path[0].y);
-      pen.path.forEach((point) => {
-        ctx.lineTo(point.x, point.y);
+      //draw pens
+      pens.forEach((pen) => {
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(pen.path[0].x, pen.path[0].y);
+        pen.path.forEach((point) => {
+          ctx.lineTo(point.x, point.y);
+        });
+        ctx.stroke();
       });
-      ctx.stroke();
-    });
-    drawFn();
-  }, []);
+
+      //draw lines
+      lines.forEach((line) => {
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(line.x1, line.y1);
+        ctx.lineTo(line.x2, line.y2);
+        ctx.stroke();
+      });
+      drawFn();
+    },
+    [lines, pens]
+  );
 
   const drawPen = useCallback(() => {
     const canvas = canvasRef.current as HTMLCanvasElement;
@@ -56,17 +71,43 @@ export default function Home() {
     }
 
     setPens([...pens]);
-  }, []);
+  }, [pens]);
+
+  const drawLine = useCallback(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) return;
+
+    if (mouseRef.current.down) {
+      const lastLine = lines[lines.length - 1];
+      lastLine.x2 = mouseRef.current.x;
+      lastLine.y2 = mouseRef.current.y;
+    } else {
+      lines.push(
+        new Line(mouseRef.current.x, mouseRef.current.y, mouseRef.current.x, mouseRef.current.y)
+      );
+    }
+
+    setLines([...lines]);
+  }, [lines]);
 
   useEffect(() => {
     initCanvas();
 
-    const interval = setInterval(() => {
-      draw(drawPen);
-    }, 1000 / 60);
+    let animateId: number;
 
-    return () => clearInterval(interval);
-  }, [initCanvas]);
+    const animate = () => {
+      draw(drawPen);
+      animateId = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      window.cancelAnimationFrame(animateId);
+    };
+  }, [draw, drawPen, initCanvas]);
 
   const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
     if (!canvasRef.current) return;
@@ -96,6 +137,7 @@ export default function Home() {
         onMouseMove={handleMouseMove}
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
       />
     </div>
   );
