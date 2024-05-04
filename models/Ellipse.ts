de76dbb55639567adb69760b8a7e59700d67e714
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-05-04 16:49:25 +0530

    referenced each shape to line for better control, and created polygon for universal shape

--- models/Ellipse.ts
+++ models/Ellipse.ts
@@ -1,47 +1,7 @@
-import { ToolColor, ToolVariant } from '@/enums/Tools';
+import { ToolVariant } from '@/enums/Tools';
+import Line from '@/models/line';
 
-class Ellipse {
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
-  radius: number;
-  color: ToolColor;
-  size: number;
-  variant: ToolVariant;
-
-  constructor(
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    radius: number,
-    color: ToolColor,
-    size: number,
-    variant: ToolVariant
-  ) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-    this.radius = radius;
-    this.color = color;
-    this.size = size;
-    this.variant = variant;
-  }
-
-  setColor(color: ToolColor) {
-    this.color = color;
-  }
-
-  setSize(size: number) {
-    this.size = size;
-  }
-
-  setVariant(variant: ToolVariant) {
-    this.variant = variant;
-  }
-
+class Ellipse extends Line {
   static drawEllipses(ellipses: Ellipse[], ctx: CanvasRenderingContext2D) {
     ellipses.forEach((ellipse) => {
       ctx.strokeStyle = ellipse.color;
@@ -54,7 +14,11 @@ class Ellipse {
         ctx.setLineDash([]);
       }
       ctx.beginPath();
-      ctx.ellipse(ellipse.x1, ellipse.y1, ellipse.x2, ellipse.y2, 0, 0, 2 * Math.PI);
+      const x = (ellipse.x1 + ellipse.x2) / 2;
+      const y = (ellipse.y1 + ellipse.y2) / 2;
+      const radiusX = Math.abs(ellipse.x1 - ellipse.x2) / 2;
+      const radiusY = Math.abs(ellipse.y1 - ellipse.y2) / 2;
+      ctx.ellipse(x, y, radiusX, radiusY, 0, 0, 2 * Math.PI);
       ctx.stroke();
     });
   }
---
+++ models/Polygon.ts
@@ -0,0 +1,59 @@
+import Line from '@/models/line';
+import { ToolColor, ToolVariant } from '@/enums/Tools';
+
+class Polygon extends Line {
+  sides: number;
+  rotation: number;
+
+  constructor(
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    color: ToolColor,
+    size: number,
+    variant: ToolVariant,
+    sides: number,
+    rotation: number
+  ) {
+    super(x1, y1, x2, y2, color, size, variant);
+    this.sides = sides;
+    this.rotation = rotation;
+  }
+
+  static drawPolygon(polygons: Polygon[], ctx: CanvasRenderingContext2D) {
+    polygons.forEach((polygon) => {
+      ctx.strokeStyle = polygon.color;
+      ctx.lineWidth = polygon.size;
+      if (polygon.variant === ToolVariant.Dashed) {
+        ctx.setLineDash([5, 3]);
+      } else if (polygon.variant === ToolVariant.Dotted) {
+        ctx.setLineDash([2, 2]);
+      } else {
+        ctx.setLineDash([]);
+      }
+      ctx.beginPath();
+      const x = (polygon.x1 + polygon.x2) / 2;
+      const y = (polygon.y1 + polygon.y2) / 2;
+      const radiusX = Math.abs(polygon.x1 - polygon.x2) / 2;
+      const radiusY = Math.abs(polygon.y1 - polygon.y2) / 2;
+
+      for (let d = 0; d <= 360; d++) {
+        if (d % (360 / polygon.sides) === 0) {
+          const a = ((d + polygon.rotation) * Math.PI) / 180;
+          const x1 = x + radiusX * Math.cos(a);
+          const y1 = y + radiusY * Math.sin(a);
+          const a2 = ((d + polygon.rotation + 360 / polygon.sides) * Math.PI) / 180;
+          const x2 = x + radiusX * Math.cos(a2);
+          const y2 = y + radiusY * Math.sin(a2);
+          ctx.moveTo(x1, y1);
+          ctx.lineTo(x2, y2);
+        }
+      }
+
+      ctx.stroke();
+    });
+  }
+}
+
+export default Polygon;
