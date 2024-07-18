8bc1c513b906360598890ea8e386d26a747224fd
Ijlal Ahmad <ijlalahmad845@gmail.com>
2024-07-18 20:17:14 +0530

    inverted favicon and brand icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
       <body className={inter.className}>
         <NextUIProvider>
--- components/IconCard.tsx
+++ components/IconCard.tsx
@@ -10,7 +10,7 @@ export default function IconCard() {
     <Card className={'absolute top-4 left-4'}>
       <CardBody className={'flex flex-row gap-4 justify-center items-center px-4'}>
         <div className={'bg-[#7728c7] rounded-xl p-1.5'}>
-          <Image src={'/img.png'} alt={''} width={28} height={28} />
+          <Image src={'/icon.png'} alt={''} width={28} height={28} />
         </div>
         <p
           className={`mb-1 text-xl ${theme === Theme.Light ? 'text-[#7728c7]' : 'text-white'} font-bold text-center select-none`}
